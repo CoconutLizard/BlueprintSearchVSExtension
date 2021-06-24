ba443128e444300b6c4608311966f9974f68b373
Davide Contini <davide.contini@coconutlizard.co.uk>
2021-06-24 16:14:11 +0100

    removed unused file

--- Source/BlueprintSearchVSExtension/Source/BlueprintSearchVSPackage.cs
+++ Source/BlueprintSearchVSExtension/Source/BlueprintSearchVSPackage.cs
@@ -53,7 +53,6 @@ namespace BlueprintSearch
 			// When initialized asynchronously, the current thread may be a background thread at this point.
 			// Do any initialization that requires the UI thread after switching to the UI thread.
 			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-			await RunScript.InitializeAsync(this);
 			await BlueprintSearchVSWindowCommand.InitializeAsync(this);
 		}
 
